fba1bb4bbf76c4e27d530f4aeac78ae344622121
Naresh <tellapuri.naresh@gmail.com>
2025-03-04 16:03:31 +0530

    added bristol role functions

--- src/main/java/com/transportdataexchange/bogdan/controller/BristolController.java
+++ src/main/java/com/transportdataexchange/bogdan/controller/BristolController.java
@@ -7,10 +7,7 @@ import com.transportdataexchange.bogdan.service.TrailerDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,6 +17,13 @@ import java.util.List;
 public class BristolController {
     @Autowired
     private TrailerDataService tdataService;
+
+    @ResponseBody
+    @GetMapping("/trailerData")
+    public List<TrailerData> getTrailerData(){
+        return  tdataService.getExceldata();
+
+    }
 
     @PostMapping("/Savetdata")
     public ResponseEntity<?> uploadtrailerData(@RequestBody List<TrailerData> updatedData) {
--- src/main/java/com/transportdataexchange/bogdan/service/TrailerDataService.java
+++ src/main/java/com/transportdataexchange/bogdan/service/TrailerDataService.java
@@ -1,10 +1,13 @@
 package com.transportdataexchange.bogdan.service;
 
+import com.transportdataexchange.bogdan.model.BusData;
 import com.transportdataexchange.bogdan.model.TrailerData;
 import com.transportdataexchange.bogdan.repository.BusDataRepository;
 import com.transportdataexchange.bogdan.repository.TrailerDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TrailerDataService {
@@ -14,4 +17,9 @@ public class TrailerDataService {
     public void savedata(TrailerData ts) {
         tdatarepo.save(ts);
     }
+
+    public List<TrailerData> getExceldata() {
+        return tdatarepo.findAll();
+    }
 }
+
